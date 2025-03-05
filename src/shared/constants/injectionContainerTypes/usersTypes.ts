0557cb31e1d97614f74e31d8550fc6565a2f6b20
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2025-03-05 11:37:54 +0300

    add dependency injection support for user management services and controllers

--- src/shared/constants/injectionContainerTypes/usersTypes.ts
+++ src/shared/constants/injectionContainerTypes/usersTypes.ts
@@ -1,4 +1,26 @@
 export const USERS_TYPES = {
   LockAccountsController: Symbol.for("LockAccountsController"),
   LockAccountService: Symbol.for("LockAccountService"),
+  BanUsersAccountsController: Symbol.for("BanUsersAccountsController"),
+  BanUserAccountService: Symbol.for("BanUserAccountService"),
+  RolesManagementController: Symbol.for("RolesManagementController"),
+  RolesManagementService: Symbol.for("RolesManagementService"),
+  UsersCrudController: Symbol.for("UsersCrudController"),
+  UsersCrudService: Symbol.for("UsersCrudService"),
+  AccountDeletionController: Symbol.for("AccountDeletionController"),
+  AccountDeletionService: Symbol.for("AccountDeletionService"),
+  AccountEmailController: Symbol.for("AccountEmailController"),
+  AccountEmailService: Symbol.for("AccountEmailService"),
+  AccountNotificationController: Symbol.for("AccountNotificationController"),
+  AccountNotificationService: Symbol.for("AccountNotificationService"),
+  AccountPasswordManagementController: Symbol.for(
+    "AccountPasswordManagementController"
+  ),
+  AccountPasswordManagementService: Symbol.for(
+    "AccountPasswordManagementService"
+  ),
+  AccountStatusController: Symbol.for("AccountStatusController"),
+  AccountStatusService: Symbol.for("AccountStatusService"),
+  ProfileController: Symbol.for("ProfileController"),
+  ProfileService: Symbol.for("ProfileService"),
 };
