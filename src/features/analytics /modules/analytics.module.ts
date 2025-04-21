7bb4ab41d633df087560bc4124d54d681061297c
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2025-04-21 02:51:09 +0200

    adding the base for analytics feature.

---
+++ src/features/analytics /modules/analytics.module.ts
@@ -0,0 +1,12 @@
+// packages imports
+import { ContainerModule, interfaces } from "inversify";
+import { Model } from "mongoose";
+
+// shard imports
+import { TYPES } from "@shared/index";
+
+/**
+ * This module encapsulates the bindings for the Content reporting feature.
+ * It defines how the services and controllers are bound to the container.
+ */
+export default new ContainerModule((bind: interfaces.Bind) => {});
